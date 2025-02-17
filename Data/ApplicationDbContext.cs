537e977350bd7efc4f8aef91af358f2ab0790a0f
kkthej <tej.kranthi06@gmail.com>
2025-02-17 22:17:54 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Pidar.Areas.Identity.Data;
+
+namespace Pidar.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<PidarUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Data/PidarDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pidar.Models;
+
+namespace Pidar.Data
+{
+    public class PidarDbContext : DbContext
+    {
+        public PidarDbContext(DbContextOptions<PidarDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Metadata> Metadata { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Metadata>(entity =>
+            {
+                entity.HasKey(e => e.DatasetId); // Explicit primary key
+                entity.Property(e => e.DatasetId)
+                      .ValueGeneratedNever(); // Disable auto-increment
+            });
+        }
+    }
+}
